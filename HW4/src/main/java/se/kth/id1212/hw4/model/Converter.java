342b8d23f8c88f315b272c71a7c7fd3aaf6fbe81
FlyHighXX <diacouthman@gmail.com>
2018-03-11 19:09:31 +0100

    finished

--- HW4/src/main/java/se/kth/id1212/hw4/model/Converter.java
+++ HW4/src/main/java/se/kth/id1212/hw4/model/Converter.java
@@ -7,9 +7,7 @@ import se.kth.id1212.hw4.view.Conversion;
  * @author Diaco Uthman
  */
 public class Converter {
-
     public double makeConversion(Conversion conversion) {
         return conversion.getAmount() * (conversion.getFromCurrency().getCurrencyIndex()/conversion.getToCurrency().getCurrencyIndex());
     }
-    
 }
